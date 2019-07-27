9dc5500fd2b604e36d81195707281819760fe7ea
nipon34.bd@gmail.com <nipon34.bd@gmail.com>
2019-07-27 18:49:50 +0600

    Add contact Us

--- app/src/main/java/net/aelbd/user/fragment/AboutUsFragment.java
+++ app/src/main/java/net/aelbd/user/fragment/AboutUsFragment.java
@@ -1,6 +1,9 @@
 package net.aelbd.user.fragment;
 
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,9 +11,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import net.aelbd.user.R;
+import net.aelbd.user.databinding.FragmentAboutUsBinding;
 
 public class AboutUsFragment extends Fragment {
 
+    private FragmentAboutUsBinding binding;
 
     public AboutUsFragment() {
 
@@ -20,7 +25,31 @@ public class AboutUsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_about_us, container, false);
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_about_us, container, false);
+
+        binding.callIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:01993342393"));
+                startActivity(intent);
+            }
+        });
+
+        binding.emailIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:" + "info@ael-bd.net"));
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
+                if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+
+        return binding.getRoot();
     }
 
 }
